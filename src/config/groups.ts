9b2a1d356735a9a07e67f77d913bd149beb99de2
lxyiii626 <3609748171@qq.com>
2026-05-10 21:52:16 +0800

    Update groups.ts

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -15,7 +15,7 @@ export const groups: Group[] = [
     name: '🔥硅碳生命体交流群',
     description: '群消息关注度权重：“user”的最新消息>其他成员最新消息>“user”的历史消息>其他成员历史消息>',
     members: [ 'ai8',  'ai6', 'ai7', 'ai9', 'ai10', 'ai5'],
-    isGroupDiscussionMode: false
+    isGroupDiscussionMode: true
   },
   /*
   {
